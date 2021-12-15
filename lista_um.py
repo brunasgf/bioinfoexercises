feea94afb2926ae15c70f018e716e6e736e2864a
Bruna Silva <brunasgf@gmail.com>
2021-12-15 12:08:12 -0300

    Add files via upload

---
+++ lista_um.py
@@ -0,0 +1,31 @@
+
+#1. printa a palavra python
+linguagem = "python"
+print(linguagem)
+
+#2. printa os calculos de a e b 
+a = 1500
+b = 335
+print(a+b)
+print(a*b)
+print(a-b)
+
+#3. exibe as palavras na mesma linha concatenando 
+a = "Marrie"
+b = " Currie "
+c = "!!! "
+print(a + b + c)
+
+#4. verifica se o resultado da soma é igual ao outro numero
+print(2 + 3 == 5)
+
+#5. dá erro de compilação pelo modo que é feito a soma
+print(5 + 9 = 13)
+
+#6. printa o valor de pi 
+pi = 3.1415 # Valor do número pi
+print(pi)
+
+# 7. com o tab dá erro na compilação 
+pi = 3.1415 # Valor do número pi
+    print(pi) # Com um "tab" antes do print(pi)
